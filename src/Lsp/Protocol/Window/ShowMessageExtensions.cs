db144f6179d7adfae73bc1838ffc7f9f1e1d0cbb
David Driscoll <david.driscoll@gmail.com>
2017-10-10 11:47:09 -0400

    Added logging extension methods

--- src/Lsp/Protocol/Window/ShowMessageExtensions.cs
+++ src/Lsp/Protocol/Window/ShowMessageExtensions.cs
@@ -11,5 +11,30 @@ namespace OmniSharp.Extensions.LanguageServer.Protocol
         {
             mediator.SendNotification("window/showMessage", @params);
         }
+
+        public static void Show(this ILanguageServer mediator, ShowMessageParams @params)
+        {
+            mediator.SendNotification("window/showMessage", @params);
+        }
+
+        public static void LogError(this ILanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Error, Message = message });
+        }
+
+        public static void Log(this ILanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Log, Message = message });
+        }
+
+        public static void LogWarning(this ILanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Warning, Message = message });
+        }
+
+        public static void LogInfo(this ILanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Info, Message = message });
+        }
     }
 }
