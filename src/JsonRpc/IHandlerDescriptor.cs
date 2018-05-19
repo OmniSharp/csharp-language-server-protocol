af67c0de7c5d80761274f3d8bba17e14416e8d19
David Driscoll <david.driscoll@gmail.com>
2018-05-18 20:44:59 -0400

    drop handler... and break all the things

--- src/JsonRpc/IHandlerDescriptor.cs
+++ src/JsonRpc/IHandlerDescriptor.cs
@@ -5,9 +5,23 @@ namespace OmniSharp.Extensions.JsonRpc
     public interface IHandlerDescriptor
     {
         string Method { get; }
-        IJsonRpcHandler Handler { get; }
         Type HandlerType { get; }
         Type Params { get; }
         Type Response { get; }
+    }
+
+    public interface IHandlerDelegateDescriptor
+    {
+        Delegate HandlerDelegate { get; }
+    }
+
+    public interface IHandlerTypeDescriptor
+    {
+        Type HandlerImplemenationType { get; }
+    }
+
+    public interface IHandlerInstanceDescriptor
+    {
+        IJsonRpcHandler Handler { get; }
     }
 }
