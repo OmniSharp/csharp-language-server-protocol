4908f44779de86048fd8ac1ec5511fd3a4ee073c
Tyler Leonhardt <tylerl0706@gmail.com>
2019-09-10 23:21:31 -0700

    JsonRpcServer.Dispose() works now

--- src/JsonRpc/OutputHandler.cs
+++ src/JsonRpc/OutputHandler.cs
@@ -23,6 +23,7 @@ namespace OmniSharp.Extensions.JsonRpc
             _serializer = serializer;
             _queue = new BlockingCollection<object>();
             _cancel = new CancellationTokenSource();
+            _outputIsFinished = new TaskCompletionSource<object>();
             _thread = new Thread(ProcessOutputQueue) { IsBackground = true, Name = "ProcessOutputQueue" };
         }
 
@@ -41,7 +42,7 @@ namespace OmniSharp.Extensions.JsonRpc
             var token = _cancel.Token;
             try
             {
-                while (true)
+                while (!token.IsCancellationRequested)
                 {
                     if (_queue.TryTake(out var value, Timeout.Infinite, token))
                     {
@@ -59,7 +60,10 @@ namespace OmniSharp.Extensions.JsonRpc
                         {
                             ms.Write(headerBytes, 0, headerBytes.Length);
                             ms.Write(contentBytes, 0, contentBytes.Length);
-                            _output.Write(ms.ToArray(), 0, (int)ms.Position);
+                            if(!token.IsCancellationRequested)
+                            {
+                                _output.Write(ms.ToArray(), 0, (int)ms.Position);
+                            }
                         }
                     }
                 }
