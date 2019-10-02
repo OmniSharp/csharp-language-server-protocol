d67b8ec3ae9ff6f2ccf860dece01070e19d732df
drsoran <drsoran@users.noreply.github.com>
2019-10-02 05:59:10 +0200

    Changes for #175 to return a Location object for diagnostic related information (#176)
    * return a Location object instead of a string for the location of a diagnostic related information
    
    * add test for related information with diagnostics

--- test/Lsp.Tests/Models/DiagnosticTests.cs
+++ test/Lsp.Tests/Models/DiagnosticTests.cs
@@ -30,6 +30,41 @@ namespace Lsp.Tests.Models
         }
 
         [Theory, JsonFixture]
+        public void RelatedInformationTest(string expected)
+        {
+            var model = new Diagnostic() {
+                Code = new DiagnosticCode("abcd"),
+                Message = "message",
+                Range = new Range(new Position(1, 1), new Position(2, 2)),
+                Severity = DiagnosticSeverity.Error,
+                Source = "csharp",
+                RelatedInformation = new Container<DiagnosticRelatedInformation>(
+                    new DiagnosticRelatedInformation {
+                        Location = new Location {
+                            Uri = new Uri("file:///abc/def.cs"),
+                            Range = new Range(new Position(1, 2), new Position(3, 4))
+                        },
+                        Message = "related message 1"
+                    },
+                    new DiagnosticRelatedInformation {
+                        Location = new Location {
+                            Uri = new Uri("file:///def/ghi.cs"),
+                            Range = new Range(new Position(5, 6), new Position(7, 8))
+                        },
+                        Message = "related message 2"
+                    }
+                )
+            };
+
+            var result = Fixture.SerializeObject(model);
+
+            result.Should().Be(expected);
+
+            var deresult = new Serializer(ClientVersion.Lsp3).DeserializeObject<Diagnostic>(expected);
+            deresult.Should().BeEquivalentTo(model);
+        }
+
+        [Theory, JsonFixture]
         public void OptionalTest(string expected)
         {
             var model = new Diagnostic();
