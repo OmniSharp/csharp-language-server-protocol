7be62da14218fd72d76f9691e8d93a5cd45775cc
David Driscoll <david.driscoll@gmail.com>
2020-04-10 00:56:19 -0400

    Try to make tests work

--- test/JsonRpc.Tests/ProcessSchedulerTests.cs
+++ test/JsonRpc.Tests/ProcessSchedulerTests.cs
@@ -161,7 +161,7 @@ namespace JsonRpc.Tests
         {
             using (IScheduler s = new ProcessScheduler(new TestLoggerFactory(_testOutputHelper), null))
             {
-                var done = new CountdownEvent(8); // 8x s.Add
+                var done = new CountdownEvent(6); // 8x s.Add
                 var running = 0;
                 var peek = 0;
 
@@ -176,14 +176,12 @@ namespace JsonRpc.Tests
                 s.Start();
                 s.Add(RequestProcessType.Serial, "bogus", handlePeek);
                 s.Add(RequestProcessType.Serial, "bogus", handlePeek);
-                s.Add(RequestProcessType.Serial, "bogus", handlePeek);
-                s.Add(RequestProcessType.Serial, "bogus", handlePeek);
                 s.Add(RequestProcessType.Parallel, "bogus", handlePeek);
                 s.Add(RequestProcessType.Serial, "bogus", handlePeek);
                 s.Add(RequestProcessType.Serial, "bogus", handlePeek);
                 s.Add(RequestProcessType.Parallel, "bogus", handlePeek);
 
-                await Task.Delay(ALONGTIME_MS);
+                await Task.Delay(ALONGTIME_MS * 2);
                 peek.Should().Be(1, because: "some tasks should overlap");
                 running.Should().Be(0, because: "all tasks have to run normally");
                 done.IsSet.Should().Be(true, because: "all tasks have to run");
@@ -197,7 +195,7 @@ namespace JsonRpc.Tests
         {
             using (IScheduler s = new ProcessScheduler(new TestLoggerFactory(_testOutputHelper), null))
             {
-                var done = new CountdownEvent(8); // 8x s.Add
+                var done = new CountdownEvent(4); // 8x s.Add
                 var running = 0;
                 var peek = 0;
 
@@ -211,10 +209,6 @@ namespace JsonRpc.Tests
 
                 s.Start();
                 s.Add(RequestProcessType.Parallel, "bogus", HandlePeek);
-                s.Add(RequestProcessType.Serial, "bogus", HandlePeek);
-                s.Add(RequestProcessType.Serial, "bogus", HandlePeek);
-                s.Add(RequestProcessType.Serial, "bogus", HandlePeek);
-                s.Add(RequestProcessType.Serial, "bogus", HandlePeek);
                 s.Add(RequestProcessType.Serial, "bogus", HandlePeek);
                 s.Add(RequestProcessType.Serial, "bogus", HandlePeek);
                 s.Add(RequestProcessType.Serial, "bogus", HandlePeek);
