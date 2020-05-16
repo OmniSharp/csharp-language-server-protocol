ed7fa960f7ccc2e6adb1dec2ca8d3eea36fa4c9e
Tyler Leonhardt <tylerl0706@gmail.com>
2020-05-16 11:45:01 -0700

    Fix FoldingRangeKind serialization to be string

---
+++ test/Lsp.Tests/Models/FoldingRangeKindTests.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Serialization;
+using Xunit;
+
+namespace Lsp.Tests
+{
+    public class FoldingRangeKindTests
+    {
+        [Fact]
+        public void CommentTest()
+        {
+            var serializer = new Serializer();
+            var json = serializer.SerializeObject(FoldingRangeKind.Comment);
+            json.Should().Be("\"comment\"");
+        }
+
+        [Fact]
+        public void ImportsTest()
+        {
+            var serializer = new Serializer();
+            var json = serializer.SerializeObject(FoldingRangeKind.Imports);
+            json.Should().Be("\"imports\"");
+        }
+
+        [Fact]
+        public void RegionTest()
+        {
+            var serializer = new Serializer();
+            var json = serializer.SerializeObject(FoldingRangeKind.Region);
+            json.Should().Be("\"region\"");
+        }
+    }
+}
