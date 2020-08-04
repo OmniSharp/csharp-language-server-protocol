899eb2f9cda3aba451715420e4fcfc3674d05851
David Driscoll <david.driscoll@gmail.com>
2020-08-04 18:22:27 -0400

    brought hack methods that were removed by accident (#276)

--- src/Protocol/Window/IShowMessageHandler.cs
+++ src/Protocol/Window/IShowMessageHandler.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using OmniSharp.Extensions.JsonRpc;
 using OmniSharp.Extensions.JsonRpc.Generation;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 
@@ -15,5 +17,33 @@ namespace OmniSharp.Extensions.LanguageServer.Protocol.Window
     public abstract class ShowMessageHandler : IShowMessageHandler
     {
         public abstract Task<Unit> Handle(ShowMessageParams request, CancellationToken cancellationToken);
+    }
+
+    public static partial class ShowMessageExtensions
+    {
+        public static void Show(this IWindowLanguageServer mediator, ShowMessageParams @params)
+        {
+            mediator.ShowMessage(@params);
+        }
+
+        public static void ShowError(this IWindowLanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Error, Message = message });
+        }
+
+        public static void Show(this IWindowLanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Log, Message = message });
+        }
+
+        public static void ShowWarning(this IWindowLanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Warning, Message = message });
+        }
+
+        public static void ShowInfo(this IWindowLanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Info, Message = message });
+        }
     }
 }
