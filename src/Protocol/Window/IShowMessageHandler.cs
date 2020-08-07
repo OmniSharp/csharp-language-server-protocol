ee5f6f9d210aa4d25b68ae7da6257d697f4297fb
David Driscoll <david.driscoll@gmail.com>
2020-08-06 23:24:37 -0400

    Added tests!

--- src/Protocol/Window/IShowMessageHandler.cs
+++ src/Protocol/Window/IShowMessageHandler.cs
@@ -21,6 +21,31 @@ namespace OmniSharp.Extensions.LanguageServer.Protocol.Window
 
     public static partial class ShowMessageExtensions
     {
+        public static void Show(this ILanguageServer mediator, ShowMessageParams @params)
+        {
+            mediator.ShowMessage(@params);
+        }
+
+        public static void ShowError(this ILanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Error, Message = message });
+        }
+
+        public static void Show(this ILanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Log, Message = message });
+        }
+
+        public static void ShowWarning(this ILanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Warning, Message = message });
+        }
+
+        public static void ShowInfo(this ILanguageServer mediator, string message)
+        {
+            mediator.ShowMessage(new ShowMessageParams() { Type = MessageType.Info, Message = message });
+        }
+
         public static void Show(this IWindowLanguageServer mediator, ShowMessageParams @params)
         {
             mediator.ShowMessage(@params);
