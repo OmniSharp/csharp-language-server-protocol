9f6264f0d886483039c7aaffd2c2c160fe9db7ec
David Driscoll <david.driscoll@gmail.com>
2020-08-25 18:31:39 -0400

    Added extension method to get all the unique handlers contained in the IHandlersManager

---
+++ test/Lsp.Tests/Integration/HandlersManagerIntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using OmniSharp.Extensions.JsonRpc;
+using OmniSharp.Extensions.JsonRpc.Testing;
+using OmniSharp.Extensions.LanguageProtocol.Testing;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Lsp.Tests.Integration
+{
+    public class HandlersManagerIntegrationTests : LanguageProtocolTestBase
+    {
+        public HandlersManagerIntegrationTests(ITestOutputHelper testOutputHelper) : base(new JsonRpcTestOptions().ConfigureForXUnit(testOutputHelper))
+        {
+        }
+
+        [Fact]
+        public async Task Should_Return_Default_Handlers()
+        {
+            var (client, server) = await Initialize(options => {}, options => {});
+
+            var handlersManager = server.GetRequiredService<IHandlersManager>();
+            handlersManager.Descriptors.Should().HaveCount(8);
+            handlersManager.GetHandlers().Should().HaveCount(5);
+        }
+
+        [Fact]
+        public async Task Should_Return_Additional_Handlers()
+        {
+            var (client, server) = await Initialize(options => {}, options => {});
+
+            server.Register(o => o.AddHandler(Substitute.For(new Type[] { typeof (ICompletionHandler), typeof(ICompletionResolveHandler) }, Array.Empty<object>()) as IJsonRpcHandler));
+            var handlersManager = server.GetRequiredService<IHandlersManager>();
+            handlersManager.Descriptors.Should().HaveCount(10);
+            handlersManager.GetHandlers().Should().HaveCount(6);
+        }
+    }
+}
