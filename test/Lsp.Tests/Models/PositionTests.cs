332e5ae6231166a95821486e5ec15d5da0683932
David Driscoll <david.driscoll@gmail.com>
2020-10-02 22:06:45 -0400

    Made position comparable (#378)

--- test/Lsp.Tests/Models/PositionTests.cs
+++ test/Lsp.Tests/Models/PositionTests.cs
@@ -20,5 +20,37 @@ namespace Lsp.Tests.Models
             var deresult = new Serializer(ClientVersion.Lsp3).DeserializeObject<Position>(expected);
             deresult.Should().BeEquivalentTo(model);
         }
+
+        [Fact]
+        public void Is_Sortable_By_Character()
+        {
+            var a = new Position(1, 1);
+            var b = new Position(1, 2);
+            var c = new Position(1, 1);
+
+            a.Should().BeLessThan(b);
+            b.Should().BeGreaterThan(a);
+
+            a.CompareTo(c).Should().Be(0);
+
+            a.Should().BeLessOrEqualTo(c);
+            a.Should().BeGreaterOrEqualTo(c);
+        }
+
+        [Fact]
+        public void Is_Sortable_By_Line()
+        {
+            var a = new Position(1, 1);
+            var b = new Position(2, 1);
+            var c = new Position(1, 1);
+
+            a.Should().BeLessThan(b);
+            b.Should().BeGreaterThan(a);
+
+            a.CompareTo(c).Should().Be(0);
+
+            a.Should().BeLessOrEqualTo(c);
+            a.Should().BeGreaterOrEqualTo(c);
+        }
     }
 }
