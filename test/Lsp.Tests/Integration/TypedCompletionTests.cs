b983447e82f32b17ca08e321516e5c5a6fbacd75
David Driscoll <david.driscoll@gmail.com>
2020-10-27 20:09:12 -0400

    intermittent test

--- test/Lsp.Tests/Integration/TypedCompletionTests.cs
+++ test/Lsp.Tests/Integration/TypedCompletionTests.cs
@@ -284,7 +284,7 @@ namespace Lsp.Tests.Integration
             item.Detail.Should().Be("resolved");
         }
 
-        [Fact]
+        [FactWithSkipOn(SkipOnPlatform.Mac)]
         public async Task Should_Resolve_With_Partial_Data_CancellationToken()
         {
             var (client, _) = await Initialize(
