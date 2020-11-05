14f5516260c71ce92de8ae5aa2f4212679d95daf
David Driscoll <david.driscoll@gmail.com>
2020-11-04 21:30:44 -0500

    Fix exception thrown by disctionary inside formatting options

--- test/Lsp.Tests/Models/FormattingOptionsTests.cs
+++ test/Lsp.Tests/Models/FormattingOptionsTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Serialization;
@@ -23,6 +24,28 @@ namespace Lsp.Tests.Models
 
             var deresult = new Serializer(ClientVersion.Lsp3).DeserializeObject<FormattingOptions>(expected);
             deresult.Should().BeEquivalentTo(model);
+        }
+
+        [Fact]
+        public void Should_Not_Require_A_Value_To_Be_Defined()
+        {
+            var model = new FormattingOptions();
+            model.InsertSpaces.Should().BeFalse();
+            model.TabSize.Should().Be(-1L);
+            model.InsertFinalNewline.Should().BeFalse();
+            model.TrimFinalNewlines.Should().BeFalse();
+            model.TrimTrailingWhitespace.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Should_Allow_Values_To_Be_Set()
+        {
+            var model = new FormattingOptions();
+            model.InsertSpaces = true;
+            model.TabSize = 4;
+            model.InsertFinalNewline = true;
+            model.TrimFinalNewlines = true;
+            model.TrimTrailingWhitespace = true;
         }
     }
 }
