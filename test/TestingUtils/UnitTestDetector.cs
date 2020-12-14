22aa7881affde0c2aea4eeab56d4a8f9c7656411
David Driscoll <david.driscoll@gmail.com>
2020-12-14 18:33:46 -0500

    detector changes

--- test/TestingUtils/UnitTestDetector.cs
+++ test/TestingUtils/UnitTestDetector.cs
@@ -7,27 +7,29 @@ namespace TestingUtils
     internal class UnitTestDetector
     {
         // ReSharper disable once InconsistentNaming
-        public static bool IsCI() => string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CI"))
-                                  && string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("TF_BUILD"));
+        public static bool IsCI() => !(
+            string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CI"))
+         && string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("TF_BUILD"))
+        );
 
         public static bool PlatformToSkipPredicate(SkipOnPlatform platform)
         {
             if (platform == SkipOnPlatform.All) return true;
             if (platform == SkipOnPlatform.None) return false;
             return Enum.GetValues(typeof(SkipOnPlatform))
-                                .OfType<SkipOnPlatform>()
-                                .Where(z => z != SkipOnPlatform.All && z != SkipOnPlatform.None)
-                                .Where(z => ( platform & z ) == z)
-                                .Any(
-                                     z => RuntimeInformation.IsOSPlatform(
-                                         platform switch {
-                                             SkipOnPlatform.Linux   => OSPlatform.Linux,
-                                             SkipOnPlatform.Mac     => OSPlatform.OSX,
-                                             SkipOnPlatform.Windows => OSPlatform.Windows,
-                                             _                      => OSPlatform.Create("Unknown")
-                                         }
-                                     )
-                                 );
+                       .OfType<SkipOnPlatform>()
+                       .Where(z => z != SkipOnPlatform.All && z != SkipOnPlatform.None)
+                       .Where(z => ( platform & z ) == z)
+                       .Any(
+                            z => RuntimeInformation.IsOSPlatform(
+                                platform switch {
+                                    SkipOnPlatform.Linux   => OSPlatform.Linux,
+                                    SkipOnPlatform.Mac     => OSPlatform.OSX,
+                                    SkipOnPlatform.Windows => OSPlatform.Windows,
+                                    _                      => OSPlatform.Create("Unknown")
+                                }
+                            )
+                        );
         }
     }
 }
