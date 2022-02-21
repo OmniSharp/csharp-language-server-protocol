e4bc72597761906bb90a7791fc5b11f33401b0c0
David Driscoll <david.driscoll@gmail.com>
2022-02-21 16:33:26 -0500

    Updated to system.reactive, collections.immutable and nerdbank.streams

--- src/JsonRpc/Serialization/Converters/EnumLikeStringConverter.cs
+++ src/JsonRpc/Serialization/Converters/EnumLikeStringConverter.cs
@@ -4,21 +4,27 @@ using Newtonsoft.Json.Linq;
 
 namespace OmniSharp.Extensions.JsonRpc.Serialization.Converters
 {
-    internal class EnumLikeStringConverter : JsonConverter<IEnumLikeString>
+    internal class EnumLikeStringConverter : JsonConverter<IEnumLikeString?>
     {
-        public override void WriteJson(JsonWriter writer, IEnumLikeString value, JsonSerializer serializer) => new JValue(value.ToString()).WriteTo(writer);
+        public override void WriteJson(JsonWriter writer, IEnumLikeString? value, JsonSerializer serializer)
+        {
+            new JValue(value?.ToString()).WriteTo(writer);
+        }
 
-        public override IEnumLikeString ReadJson(
-            JsonReader reader, Type objectType, IEnumLikeString existingValue,
+        public override IEnumLikeString? ReadJson(
+            JsonReader reader, Type objectType, IEnumLikeString? existingValue,
             bool hasExistingValue,
             JsonSerializer serializer
-        ) =>
-            ( reader.TokenType, Nullable.GetUnderlyingType(objectType) ) switch {
-                (JsonToken.String, null)         => (IEnumLikeString) Activator.CreateInstance(objectType, (string) reader.Value),
-                (JsonToken.String, { } realType) => (IEnumLikeString) Activator.CreateInstance(realType, (string) reader.Value),
-                (_, { })                         => (IEnumLikeString) Activator.CreateInstance(objectType, null),
+        )
+        {
+            return ( reader.TokenType, Nullable.GetUnderlyingType(objectType) ) switch
+            {
+                (JsonToken.String, null)         => (IEnumLikeString)Activator.CreateInstance(objectType, (string)reader.Value),
+                (JsonToken.String, { } realType) => (IEnumLikeString)Activator.CreateInstance(realType, (string)reader.Value),
+                (_, { })                         => (IEnumLikeString)Activator.CreateInstance(objectType, null),
                 _                                => null
             };
+        }
 
         public override bool CanRead => true;
     }
