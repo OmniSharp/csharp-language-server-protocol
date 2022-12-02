aac9b5986b5bba08ce8fedd94871a067acea2fd5
Andy Jordan <2226434+andschwa@users.noreply.github.com>
2022-12-02 11:33:03 -0800

    Fix type forwarding for `init`/`record` workaround (#895)
    * Update `nuke.globaltool` to fix macOS CI
    
    * Revert "disable macos builds for now"
    
    This reverts commit 82934b6ebd417cd875c81c9b639c5b58b8866175 and
    commit 9b583ba8319534b1d704b7d3ba2d63fb8d67b6ce.
    
    * Fix type forwarding for `init` and `record` workaround
    
    * Only target `net6.0` for tests
    
    So they don't need `IsExternalInit` defined too.

--- src/JsonRpc.Generators/Records.cs
+++ src/JsonRpc.Generators/Records.cs
@@ -1,6 +1,8 @@
 #pragma warning disable MA0048 // File name must match type name
-#define INTERNAL_RECORD_ATTRIBUTES
-#if NETSTANDARD2_0 ||  NETSTANDARD2_1 ||  NETCOREAPP2_0 ||  NETCOREAPP2_1 ||  NETCOREAPP2_2 || NET45 || NET451 || NET452 || NET6 || NET461 || NET462 || NET47 || NET471 || NET472 || NET48
+#if NET5_0_OR_GREATER
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Runtime.CompilerServices.IsExternalInit))]
+#else
+
 using System.ComponentModel;
 
 // ReSharper disable once CheckNamespace
@@ -11,12 +13,7 @@ namespace System.Runtime.CompilerServices
     /// This class should not be used by developers in source code.
     /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
-#if INTERNAL_RECORD_ATTRIBUTES
-    internal
-#else
-    public
-#endif
-        static class IsExternalInit
+    internal static class IsExternalInit
     {
     }
 }
